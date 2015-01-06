fa35f883ef079fa2ffe02d1df5c9438d94da3c5e
Steven Williamson <steve@freeagent.com>
2015-01-06 12:59:11 +0000

    Imported some mysqld dtrace scripts

---
+++ mysqld/mysql_pid_latency.d
@@ -0,0 +1,39 @@
+#!/usr/sbin/dtrace -s
+/*
+ * mysql_pid_latency.d  Print query latency distribution every second.
+ *
+ * USAGE: ./mysql_pid_latency.d -p mysqld_PID
+ *
+ */
+
+#pragma D option quiet
+
+dtrace:::BEGIN
+{
+        printf("Tracing PID %d... Hit Ctrl-C to end.\n", $target);
+}
+
+mysql$target::*dispatch_command*:query-start
+{
+        self->start = timestamp;
+}
+
+mysql$target::*dispatch_command*:query-done
+/self->start/
+{
+        @time = quantize(timestamp - self->start);
+        @num = count();
+        self->start = 0;
+}
+
+profile:::tick-1s
+{
+        printa("\nMySQL queries/second: %@d; query latency (ns):", @num);
+        printa(@time);
+        clear(@time); clear(@num);
+}
+
+dtrace:::END
+{
+        trunc(@time); trunc(@num);
+}
---
+++ mysqld/mysql_pid_slow.d
@@ -0,0 +1,49 @@
+#!/usr/sbin/dtrace -s
+/*
+ * mysql_pid_slow.d     Trace queries slower than specified ms.
+ *
+ * USAGE: ./mysql_pid_slow.d -p mysqld_PID min_ms
+ *
+ */
+
+#pragma D option quiet
+#pragma D option defaultargs
+#pragma D option switchrate=10hz
+#pragma D option strsize=8000
+
+dtrace:::BEGIN
+/$1 == 0/
+{
+        printf("USAGE: %s -p PID min_ms\n\n", $$0);
+        printf("\teg: %s -p 12345 100\n", $$0);
+        exit(1);
+}
+
+dtrace:::BEGIN
+{
+        min_ns = $1 * 1000000;
+        printf("Tracing... Min query time: %d ns.\n\n", min_ns);
+        printf(" %-8s %-8s %s\n", "TIME(ms)", "CPU(ms)", "QUERY");
+}
+
+mysql$target::*dispatch_command*:query-start
+{
+        self->query = copyinstr(arg0);
+        self->start = timestamp;
+        self->vstart = vtimestamp;
+}
+
+mysql$target::*dispatch_command*:query-done
+/self->start && (timestamp - self->start) > min_ns/
+{
+        this->time = (timestamp - self->start) / 1000000;
+        this->vtime = (vtimestamp - self->vstart) / 1000000;
+        printf(" %-8d %-8d %S\n", this->time, this->vtime, self->query);
+}
+
+mysql$target::*dispatch_command*:query-done
+{
+        self->query = 0;
+        self->start = 0;
+        self->vstart = 0;
+}
