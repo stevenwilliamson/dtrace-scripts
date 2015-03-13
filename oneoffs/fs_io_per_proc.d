aad80706a5dd2d584b023dfd415e40977cfc47de
Steven Williamson <steve@freeagent.com>
2015-03-13 10:44:56 +0000

    Tidy up scripts

--- oneoffs/fs_io_per_proc.d
+++ oneoffs/fs_io_per_proc.d
@@ -1,10 +1,20 @@
+#!/usr/sbin/dtrace -s
+
+/* Number of IOPS per process, printed every 10 seconds */
 syscall::read:entry /fds[arg0].fi_fs == "zfs"/
 {
-    @data[execname] = count();
+    @read[execname] = count();
+}
+
+syscall::write:entry /fds[arg0].fi_fs == "zfs"/
+{
+    @write[execname] = count();
 }
 
 tick-10s
 {
-  printa(@data);
-  exit(0);
+  printa(@read);
+  printa(@write);
+  clear(@read);
+  clear(@write);
 }
