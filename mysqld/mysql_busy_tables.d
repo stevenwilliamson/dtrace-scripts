32bb124f9db0f3d0e03fed085895e0441ed6b88f
Steven Williamson <steve@freeagent.com>
2015-10-03 09:53:03 +0100

    WIP figure out busy tables

---
+++ mysqld/mysql_busy_tables.d
@@ -0,0 +1,30 @@
+#!/usr/sbin/dtrace -s
+
+
+/* Reports aggregations of table access, though it is not 100% accurate.
+ *
+ * We hook in to the MySQL function check_table_name which is called to check a table
+ * name is valid. This is done for each query so it does give us a good view of
+ * which tables are been accessed often.
+ * */
+
+/*
+ * usage:
+ * mysql_busy_tables.d -p PID
+ */
+
+#pragma D option quiet
+dtrace:::BEGIN
+{
+  printf("Queries per tabe\n");
+}
+
+pid$target::*check_table_name*:entry
+{
+  @tables[stringof(copyin(arg0, arg1))] = count();
+}
+
+tick-1s {
+  printa(@tables);
+  trunc(@tables);
+}
